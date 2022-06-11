881a9c14c8042d4fd0130d8bec9743a6cd169ab6
Ian Park <ian.park@sodacrew.com>
2022-06-11 18:38:21 +0900

    Modify: Button Prop Children type에 따라 return 값 수정

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { ReactChildren } from 'react';
 import styled from 'styled-components/native';
 
 import { flexBox } from '../styles/utils';
@@ -9,26 +9,43 @@ type ButtonFontWeight = 300 | 400 | 500 | 600 | 700 | 900;
 
 type ButtonProps = {
   variant: ButtonVariant;
-  children: string;
   size: '100%';
+  children?: ReactChildren | string;
   fontWeight?: ButtonFontWeight;
   onPress?: () => void;
 };
 
+// TODO: children 일 때랑 일반 text가 들어올 때랑 다르게 만들어야 할 것 같다.
+// ex) Facebook 로그인 아이콘이 들어온다면 Text 안에서는 안될 것 같으니까.
+// 어느정도 추상화를 해야 할까??
+
 function Button(props: ButtonProps) {
   const { variant, children, size, fontWeight, onPress, ...others } = props;
 
+  if (typeof children === 'string')
+    return (
+      <Styled.Button
+        variant={variant}
+        size={size}
+        onPress={onPress}
+        {...others}
+      >
+        <Styled.Text variant={variant} fontWeight={fontWeight}>
+          {children}
+        </Styled.Text>
+      </Styled.Button>
+    );
+
   return (
     <Styled.Button variant={variant} size={size} onPress={onPress} {...others}>
-      <Styled.Text variant={variant} fontWeight={fontWeight}>
-        {children}
-      </Styled.Text>
+      {children}
     </Styled.Button>
   );
 }
 
 export default Button;
 
+// Styled
 type StyledButtonProps = {
   variant: ButtonVariant;
   size?: ButtonSize;
