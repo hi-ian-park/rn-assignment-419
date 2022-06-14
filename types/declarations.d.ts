d16e6f5172f0df10f42dd8033d1192b7f0aef6fa
Ian Park <ian.park@sodacrew.com>
2022-06-14 14:18:02 +0900

    WIP: svg to icon 진행 중 실패

---
+++ types/declarations.d.ts
@@ -0,0 +1,7 @@
+// TODO: svg to ICON 진행하기
+declare module '*.svg' {
+  import React from 'react';
+  import { SvgProps } from 'react-native-svg';
+  const content: React.FC<SvgProps>;
+  export default content;
+}
