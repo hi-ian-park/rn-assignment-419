002e843ab5feca923f13e6464d5de008fbc43f14
Ian Park <ian.park@sodacrew.com>
2022-06-20 11:31:42 +0900

    Test: Login 통신 테스트

--- screens/auth/Login.tsx
+++ screens/auth/Login.tsx
@@ -2,9 +2,7 @@ import React, { useState } from 'react';
 import { SafeAreaView, TouchableOpacity } from 'react-native';
 import styled from 'styled-components/native';
 
-import NavigationBar from 'components/Bars/NavigationBar';
 import Btn from 'components/Btn';
-import InputBase from 'components/Input';
 import Text from 'components/Text';
 import { theme } from 'styles/theme';
 
@@ -13,13 +11,29 @@ import PasswordInput from '../../components/Input/Password';
 function Login({ route }) {
   // TODO: navigate route로 email 보내줘야함~!
   const [password, setPassword] = useState('');
-  const [hidePassword, setHidePassword] = useState(true);
   const onChangeText = (text) => setPassword(text);
+  const handlePressLoginBtn = async () => {
+    console.log({
+      email: route.params.email,
+      password,
+    });
+    const response = await fetch('https://auth-dev.sodacrew.com/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: route.params.email,
+        password,
+      }),
+    });
+    const data = await response.json();
+    console.log(data);
+  };
   console.log(route);
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <Styled.Container>
-        <NavigationBar />
         <Text size="lg" fontWeight="bold">
           Hi, {route.params.name}
         </Text>
@@ -33,22 +47,7 @@ function Login({ route }) {
           placeholderTextColor="#7b7b7b"
           textContentType="password"
         />
-        {/* <Styled.InputWrapper>
-          <InputBase
-            placeholder="Password"
-            placeholderTextColor="#7b7b7b"
-            secureTextEntry={hidePassword}
-            textContentType="password"
-          />
-          <Styled.HelpText>
-            <TouchableOpacity onPress={handleShowPassword}>
-              <Text size="sm" style={{ textDecorationLine: 'underline' }}>
-                {hidePassword ? 'Show password' : 'Hide password'}
-              </Text>
-            </TouchableOpacity>
-          </Styled.HelpText>
-        </Styled.InputWrapper> */}
-        <Btn size="100%" variant="primary">
+        <Btn size="100%" variant="primary" onPress={handlePressLoginBtn}>
           <Text fontWeight="bold" color={theme.colors.white}>
             Login
           </Text>
@@ -66,7 +65,7 @@ function Login({ route }) {
 const Styled = {
   Container: styled.View`
     flex: 1;
-    padding: 0 16px;
+    padding: 32px 16px 0 16px;
   `,
 
   Text: styled.Text`
