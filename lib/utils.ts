215e1760fa59a5ae15ab54c32278e9a41261669a
hi-ian-park <103913438+hi-ian-park@users.noreply.github.com>
2022-07-06 13:57:03 +0900

    Remove: unused function

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,16 +4,4 @@ export default {
       setTimeout(resolve, t);
     });
   },
-
-  getErrorMessage: (error: unknown) => {
-    if (error instanceof Error) return error.message;
-    return String(error);
-  },
-
-  reportError: ({ message }: { message: string }) => {
-    // sentry error reporting
-    // Sentry.captureException(error);
-    // toast.error(message);
-    console.log(message);
-  },
 };
